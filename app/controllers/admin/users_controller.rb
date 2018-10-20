2259b0fd4a36e1c3ababa2c9cd78232dfc98f4b1
shgtkshruch <shgtk.shruch@gmail.com>
2018-10-20 15:31:20 +0900

    Add user create form

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,30 @@
 class Admin::UsersController < ApplicationController
-  def new
-  end
-
-  def edit
+  def index
   end
 
   def show
   end
 
-  def index
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirm)
   end
 end
