c96d6b09bc9c3fd728cde7002b8a9b5bbe9b9f44
shgtkshruch <shgtk.shruch@gmail.com>
2018-10-21 11:34:35 +0900

    Refactoring spec with let

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,31 +2,27 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    let(:user_a) { create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
     before do
-      user_a = create(:user, name: 'ユーザーA', email: 'a@example.com')
       create(:task, name: '最初のタスク', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'ユーザーAがログインしているとき' do
-      before do
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_a }
 
       it 'ユーザーAが作成したタスクが表示される' do
         expect(page).to have_content '最初のタスク'
       end
     end
+
     context 'ユーザーBがログインしているとき' do
-      before do
-        create(:user, name: 'ユーザーB', email: 'b@example.com')
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_b }
 
       it 'ユーザーAが作成したタスクが表示されない' do
         expect(page).to have_no_content '最初のタスク'
